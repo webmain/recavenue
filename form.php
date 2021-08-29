ca0b02a3ac14c868204a1bdde1b4b4b68f83eddf
webmain <89526614+webmain@users.noreply.github.com>
2021-08-29 20:09:55 +0200

    Add files via upload

--- form.php
+++ form.php
@@ -1,35 +1,27 @@
 <link href="style.css" rel="stylesheet">
 <link rel="icon" href="LOGO.ico">
 <?php
-
-$nom = $_POST['nom'];        						
-$prenom = $_POST['prenom'];                       
-$numero = $_POST['numero'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-       
-
+     $nom = $_POST['nom'];
+     $prenom = $_POST['prenom'];
+     $numero = $_POST['numero'];
+     $email = $_POST['email'];
+     $message = $_POST['message'];
+     
 $mail_entete  = 'MIME-Version: 1.0'."\n";
-$mail_entete .= 'From: "$nom et $prenom" <mahieurobin2@gmail.com>'."\n";
+$mail_entete .= 'From: "Nom" <mahieurobin2@gmail.com>'."\n";
 $mail_entete .= 'Reply-To: mahieurobin2@gmail.com'."\r\n";
 $mail_entete .= 'Content-Type: text/html; charset="iso-8859-1"'."\n";
 $mail_entete .= 'Content-Transfer-Encoding: 8bit';
 
 
-$mail_corps = "$nom $prenom\n";
-$mail_corps .= "$email $numero\n";
-$mail_corps .= "$message\n";
+// préparation du corps du mail
+$mail_corps = "Message de : $Prenom $Nom\n";
+$mail_corps .="Numero de telephone : $numero Adresse mail : $email\n";
+$mail_corps .="$message";
 
-
-if (mail('mahieurobin2@gmail.com','Reponses Formulaire-Sondage',$mail_corps,$mail_entete)) {
-
-    echo "Votre message a bien etait envoyé nous vous répondrons dans des delais les plus breves, merci par avance pour votre patience. Bien cordialement L'équipe rec avenue";
-
-
+if (mail('mahieurobin2@gmail.com','Message du formulaire de presentation',$mail_corps,$mail_entete)) {
+echo "ok";
 } else {
-
- echo "Une erreur est survenue lors de l'envoi du message veuillez reesayer dans quelques minutes";
-
+echo "erreur";
 }
-
 ?>
